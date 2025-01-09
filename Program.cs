110ea2ff5b56a232177f24e1e4f713bf9aa5fc63
nicorasck <nicholas.rasck@gmail.com>
2025-01-09 13:51:10 +0100

    Updated - facctory

--- Program.cs
+++ Program.cs
@@ -7,38 +7,41 @@ var options = new DbContextOptionsBuilder<StudentDbContext>()
 .UseSqlite("Data Source = StudentDb.db")
 .Options;
 
-using var context = new StudentDbContext(options);
-
-if (!context.Schools.Any())
+var factory = new StudentContextFactory();
+using (var context = factory.CreateDbContext(args))
 {
-    System.Console.WriteLine("No schools are registered, adding the first.");
-    System.Console.WriteLine("Enter the School Name:");
-    var newSchoolName = Console.ReadLine();
-    context.Schools.Add(new School {Name = newSchoolName, Address = "Malmvaegen 1"});
 
-    context.SaveChanges();
-    System.Console.WriteLine("Database is updated.");
-}
+    if (!context.Schools.Any())
+    {
+        System.Console.WriteLine("No schools are registered, adding the first.");
+        System.Console.WriteLine("Enter the School Name:");
+        var newSchoolName = Console.ReadLine();
+        context.Schools.Add(new School { Name = newSchoolName, Address = "Malmvaegen 1" });
 
-var schools = context.Schools.ToList();
-foreach (var school in schools)
-{
-    System.Console.WriteLine($"School Name: {school.Name}, Address: {school.Address}");
-}
+        context.SaveChanges();
+        System.Console.WriteLine("Database is updated.");
+    }
 
-if (!context.Students.Any())
-{
-    context.Students.Add(new Student {Name = "Jojje", Email = "Jojje99@mail.com"});
-    context.Students.Add(new Student {Name = "Nico", Email = "Nico89@mail.com"});
+    var schools = context.Schools.ToList();
+    foreach (var school in schools)
+    {
+        System.Console.WriteLine($"School Name: {school.Name}, Address: {school.Address}");
+    }
 
-    context.SaveChanges();
-    System.Console.WriteLine("Database is updated.");
-}
+    if (!context.Students.Any())
+    {
+        context.Students.Add(new Student { Name = "Jojje", Email = "Jojje99@mail.com" });
+        context.Students.Add(new Student { Name = "Nico", Email = "Nico89@mail.com" });
 
-System.Console.WriteLine("Loading students from DB:n\n");
-var students = context.Students.ToList();
+        context.SaveChanges();
+        System.Console.WriteLine("Database is updated.");
+    }
 
-foreach (var student in students)
-{
-    System.Console.WriteLine($"Student Name: {student.Name}, Email: {student.Email}, StudentID: {student.StudentID}");
+    System.Console.WriteLine("Loading students from DB:n\n");
+    var students = context.Students.ToList();
+
+    foreach (var student in students)
+    {
+        System.Console.WriteLine($"Student Name: {student.Name}, Email: {student.Email}, StudentID: {student.StudentID}");
+    }
 }
